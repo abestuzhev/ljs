f81324b318584918bdc9bc2fc6907c01ef5ca088
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-03-03 23:01:50 +0300

    changes

--- base.js
+++ base.js
@@ -1,6 +1,32 @@
 
+/*5. замыкания, область видимости--------------------------------*/
+console.log("6. Методы объектов и контекст вызова");
+console.log("-----------------------------------------------");
+/*------------------------------------------------------------*/
 
+/*5. замыкания, область видимости--------------------------------*/
+console.log("5. замыкания, область видимости");
+var phrase = "Hello"
+function sayHi(name){
+  console.log(phrase + ' ' + name);
+}
+sayHi('John');
+phrase = "Goodbye"
+sayHi('Bob');
 
-/*замыкания, область видимости--------------------------------*/
+function sayHiBye(firstName, lastName){
+  // LexicalEnvironment
+  //getFullName.[[Scope]] = объект переменных текущего запуска sayHiBye
+  console.log("Hello " + getFullName());
+  console.log("Goodbye " + getFullName());
 
+  function getFullName (){
+    // LexicalEnvironment
+    return firstName + ' ' + lastName;
+  }
+
+}
+
+sayHiBye('John', "Doe");
+console.log("-----------------------------------------------");
 /*------------------------------------------------------------*/
